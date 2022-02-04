b7f500e6fcaa1ce2f5a0b6b46d95652e152160ba
Philip Hughes <phughes@octolabs.io>
2022-02-04 14:25:14 -0500

    replace zerolog

--- zerolog/sentry_writer_test.go
+++ zerolog/sentry_writer_test.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/getsentry/sentry-go"
 	"github.com/rs/zerolog"
+
 	"github.com/secureworks/logger/internal/common"
 	"github.com/secureworks/logger/internal/testutils"
 	"github.com/secureworks/logger/log"
-	"github.com/stretchr/testify/require"
 )
 
 func TestZerolog_SentryWriter_CheckLevel(t *testing.T) {
@@ -18,13 +18,13 @@ func TestZerolog_SentryWriter_CheckLevel(t *testing.T) {
 
 		zlvl, ok := writer.checkLevel(
 			[]byte(`{"message":"test message here","error":"error message"}`))
-		require.False(t, ok)
-		require.Equal(t, zerolog.Level(0), zlvl)
+		testutils.AssertFalse(t, ok)
+		testutils.AssertEqual(t, zerolog.Level(0), zlvl)
 
 		zlvl, ok = writer.checkLevel(
 			[]byte(`"message":"test message here ...`))
-		require.False(t, ok)
-		require.Equal(t, zerolog.Level(0), zlvl)
+		testutils.AssertFalse(t, ok)
+		testutils.AssertEqual(t, zerolog.Level(0), zlvl)
 	})
 
 	t.Run("level found but is too low", func(t *testing.T) {
@@ -32,13 +32,13 @@ func TestZerolog_SentryWriter_CheckLevel(t *testing.T) {
 
 		zlvl, ok := writer.checkLevel(
 			[]byte(`{"message":"test message here","error":"error message","level":"warn"}`))
-		require.False(t, ok)
-		require.Equal(t, zerolog.WarnLevel, zlvl)
+		testutils.AssertFalse(t, ok)
+		testutils.AssertEqual(t, zerolog.WarnLevel, zlvl)
 
 		zlvl, ok = writer.checkLevel(
 			[]byte(`"level":"warn","message":"test message here ...`))
-		require.False(t, ok)
-		require.Equal(t, zerolog.WarnLevel, zlvl)
+		testutils.AssertFalse(t, ok)
+		testutils.AssertEqual(t, zerolog.WarnLevel, zlvl)
 	})
 
 	t.Run("level is found and meets threshold", func(t *testing.T) {
@@ -46,19 +46,17 @@ func TestZerolog_SentryWriter_CheckLevel(t *testing.T) {
 
 		zlvl, ok := writer.checkLevel(
 			[]byte(`{"message":"test message here","error":"error message","level":"error"}`))
-		require.True(t, ok)
-		require.Equal(t, zerolog.ErrorLevel, zlvl)
+		testutils.AssertTrue(t, ok)
+		testutils.AssertEqual(t, zerolog.ErrorLevel, zlvl)
 
 		zlvl, ok = writer.checkLevel(
 			[]byte(`"level":"error","message":"test message here ...`))
-		require.True(t, ok)
-		require.Equal(t, zerolog.ErrorLevel, zlvl)
+		testutils.AssertTrue(t, ok)
+		testutils.AssertEqual(t, zerolog.ErrorLevel, zlvl)
 	})
 }
 
 func TestZerolog_SentryWriter_Write(t *testing.T) {
-	require := require.New(t)
-
 	srv, sentryMsg := testutils.SentryServer(t, false)
 	defer srv.Close()
 
@@ -70,15 +68,15 @@ func TestZerolog_SentryWriter_Write(t *testing.T) {
 	writer := newSentryWriter(log.ERROR, log.PANIC, log.FATAL)
 	n, err := writer.Write(
 		[]byte(`{"message":"test message here","error":"error message","level":"error","panic_value":"panic message"}`))
-	require.Greater(n, 0)
-	require.NoError(err)
+	testutils.AssertTrue(t, n > 0)
+	testutils.AssertNil(t, err)
 
 	var event *sentry.Event
 	err = json.Unmarshal(sentryMsg(t), &event)
-	require.NoError(err)
+	testutils.AssertNil(t, err)
 
-	require.NotNil(event)
-	require.Len(event.Exception, 2)
-	require.Equal("error message", event.Exception[0].Value)
-	require.Equal("panic message", event.Exception[1].Value)
+	testutils.AssertNotNil(t, event)
+	testutils.AssertEqual(t, 2, len(event.Exception))
+	testutils.AssertEqual(t, "error message", event.Exception[0].Value)
+	testutils.AssertEqual(t, "panic message", event.Exception[1].Value)
 }
--- zerolog/zerolog_unsafe_test.go
+++ zerolog/zerolog_unsafe_test.go
@@ -8,60 +8,57 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/secureworks/logger/log"
-
 	"github.com/rs/zerolog"
-	"github.com/stretchr/testify/require"
+
+	"github.com/secureworks/logger/internal/testutils"
+	"github.com/secureworks/logger/log"
 )
 
 func TestZerolog_UnsafeEventLevelChange(t *testing.T) {
-	require := require.New(t)
-
-	// 'blank'/new event defaults to Debug in zerolog
-	// there is no way to create a new Event with something else
-	// outside of chaining one from a logger with a different level
+	// A "blank," new Zerolog Event defaults to Debug. There is no way to
+	// create a new Event with something else other than chaining one from a
+	// logger with a different level.
 	event := zerolog.Dict()
 
-	// Since there is also no method/field for getting the level itself
-	// we will use reflect. You can use reflect to (indirectly) read the value of a
-	// private field, but you cannot use reflect to set it.
-	// While some operations below are unsafe in their own right
-	// the point of this test is to alert us when something in zerolog changes
-	// so any panic/failure is fine.
+	// Since there is also no method / field for getting the level itself we
+	// will use reflect. You can use reflect to (indirectly) read the value
+	// of a private field, but you cannot use reflect to set it. While some
+	// operations below are unsafe in their own right the point of this test
+	// is to alert us when something in zerolog changes so any panic/failure
+	// is fine.
 	val := reflect.ValueOf(event).Elem().FieldByName("level")
-	require.True(val.IsValid(), "cannot find 'level' struct field in zerolog.Event")
+	testutils.AssertTrue(t, val.IsValid())
 
-	// Make sure we have expected default value, and fields line up
-	// Cannot use String method, but we can use Int
-	require.Equal(int64(zerolog.DebugLevel), val.Int())
+	// Make sure we have the expected default value, and that fields line
+	// up. Cannot use String method, but we can use Int.
+	testutils.AssertEqual(t, int64(zerolog.DebugLevel), val.Int())
 
-	// Change field
+	// Change field and ensure correct.
 	changeEventLevel(event, zerolog.WarnLevel)
-
-	// Make sure change is correct
-	require.Equal(int64(zerolog.WarnLevel), val.Int())
-}
-
-// checkLevel implements the zerolog.Hook interface to make sure
-// the expected level is passed when Run.
-type checkLevel struct {
-	req     *require.Assertions
-	toCheck zerolog.Level
-}
-
-func (cl *checkLevel) Run(e *zerolog.Event, lvl zerolog.Level, msg string) {
-	cl.req.Equal(cl.toCheck, lvl, "incorrect log level")
+	testutils.AssertEqual(t, int64(zerolog.WarnLevel), val.Int())
 }
 
 func TestZerolog_ExpectedHookBehavior(t *testing.T) {
-	require := require.New(t)
+	hook := &checkLevelHook{}
+	logger, err := log.Open(
+		"zerolog",
+		nil,
+		log.CustomOption("Hook", hook),
+		log.CustomOption("Output", ioutil.Discard),
+	)
+	testutils.AssertNil(t, err)
 
-	zlog, err := log.Open("zerolog", nil, log.CustomOption(
-		"Hook",
-		&checkLevel{req: require, toCheck: zerolog.ErrorLevel},
-	), log.CustomOption("Output", ioutil.Discard))
-	require.Nil(err)
+	// Hook will run and store the error value passed.
+	logger.Error().Msg("foobar")
+	testutils.AssertEqual(t, zerolog.ErrorLevel, hook.EventLevel)
+}
 
-	// Hook will run and check values
-	zlog.Error().Msg("foobar")
+// checkLevelHook implements the zerolog.Hook interface to make sure
+// the expected level is passed when Run.
+type checkLevelHook struct {
+	EventLevel zerolog.Level
+}
+
+func (cl *checkLevelHook) Run(_ *zerolog.Event, lvl zerolog.Level, _ string) {
+	cl.EventLevel = lvl
 }
