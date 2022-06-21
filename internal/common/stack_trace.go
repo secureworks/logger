d2b5e7dc4a502dfca956deffbcf2c8b356f395ad
Anthony Carathimas <acarathimas@octolabs.io>
2022-06-21 18:06:56 -0400

    increased stack trace skip by one

--- internal/common/stack_trace.go
+++ internal/common/stack_trace.go
@@ -46,7 +46,7 @@ func WithStack(err error) error {
 func callers() *stack {
 	const depth = 32
 	var pcs [depth]uintptr
-	n := runtime.Callers(6, pcs[:])
+	n := runtime.Callers(7, pcs[:])
 	var st stack = pcs[0:n]
 	return &st
 }
