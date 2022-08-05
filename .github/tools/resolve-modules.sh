dea806e67e4852d473b32056b7762cc7bd39a753
Philip Hughes <phughes@octolabs.io>
2022-08-05 15:13:37 -0400

    add module resolver and lint matrix

---
+++ .github/tools/resolve-modules.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Recursively finds all directories with a go.mod file and creates
+# a GitHub Actions JSON output option. This is used by the linter action.
+
+echo "Resolving modules in $(pwd)"
+
+PATHS=$(find . -type f -name go.mod | awk '{ printf "{\"workdir\":\"%s\"},", $1; }')
+echo "::set-output name=matrix::{\"include\":[${PATHS%?}]}"
